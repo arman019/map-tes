e598260db2b646e0411a07e39c0785ec06551f17
Inzamamul haque <inzamamul.softbdltd@gmail.com>
2022-09-29 09:47:17 +0600

    init

---
+++ data/bridge.js
@@ -0,0 +1,10 @@
+
+    var bridgeJSON ={
+        "type": "FeatureCollection",
+        "name": "Bridge",
+        "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+        "features": [
+        { "type": "Feature", "properties": { "FID_": 0, "Entity": "LWPolyline", "Layer": "Bridge", "Color": 1, "Linetype": "CONTINUOUS", "Elevation": 0.0, "LineWt": 30, "RefName": null }, "geometry": { "type": "LineString", "coordinates": [ [ 90.292104658109622, 22.360591679937457, 0.0 ], [ 90.29210193959031, 22.360556964758164, 0.0 ], [ 90.292342496906542, 22.360555583524, 0.0 ], [ 90.292342621099365, 22.360590805932208, 0.0 ], [ 90.292104658109622, 22.360591679937457, 0.0 ] ] } },
+        { "type": "Feature", "properties": { "FID_": 0, "Entity": "LWPolyline", "Layer": "Bridge", "Color": 1, "Linetype": "CONTINUOUS", "Elevation": 0.0, "LineWt": 30, "RefName": null }, "geometry": { "type": "LineString", "coordinates": [ [ 90.290608539294894, 22.358619741634911, 0.0 ], [ 90.290620584441825, 22.358637041681042, 0.0 ], [ 90.290748378763382, 22.358554093747326, 0.0 ], [ 90.29073633360548, 22.358536793709707, 0.0 ], [ 90.290608539294894, 22.358619741634911, 0.0 ] ] } }
+        ]
+        }
